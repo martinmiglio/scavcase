aa3f0c7d4fbcdc721484fe68ba05cc9d5c01d36b
Martin Miglio <marmig0404@gmail.com>
2023-09-09 02:57:44 -0400

    add data attribution

--- src/components/page/FooterBar.tsx
+++ src/components/page/FooterBar.tsx
@@ -3,7 +3,13 @@ import Link from "next/link";
 export default function FooterBar() {
   return (
     <footer className="flex w-full flex-row flex-wrap items-center justify-between py-8 text-sm opacity-60">
-      <div className="pb-1">
+      <div>
+        Data from{" "}
+        <Link href="https://tarkov.dev/" className="hover:underline">
+          tarkov.dev
+        </Link>
+      </div>
+      <div>
         © 2023{" "}
         <Link href="https://martinmiglio.dev/" className="hover:underline">
           Martin Miglio
