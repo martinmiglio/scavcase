27e21bf23290461fb6a2d142329fbe0ac2980a35
Martin Miglio <marmig0404@gmail.com>
2023-09-10 15:23:24 -0400

    make header image priority

--- src/components/page/NavBar.tsx
+++ src/components/page/NavBar.tsx
@@ -1,13 +1,13 @@
 import logo from "@/../public/icon.svg";
-import Image from "@/components/atomic/Image";
 import AccountButton from "@/components/auth/AccountButton";
+import Image from "next/image";
 import Link from "next/link";
 
 const ICON_SIZE = 50;
 
 export default function NavBar({ title }: { title: string }) {
   return (
-    <nav className="relative mb-2 flex w-full justify-between bg-background px-4 py-2  shadow-lg">
+    <nav className="relative mb-2 flex w-full justify-between bg-background px-4 py-2 shadow-lg">
       <Link href="/">
         <header className="flex items-center gap-2">
           <Image
@@ -15,6 +15,7 @@ export default function NavBar({ title }: { title: string }) {
             height={ICON_SIZE}
             width={ICON_SIZE}
             alt="scavcase icon"
+            priority
           />
           <h1 className="text-3xl font-bold">{title}</h1>
         </header>
