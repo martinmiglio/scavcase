e3f4f4b439ea64fc3d12c12a8932ec17f1d9a323
Martin Miglio <marmig0404@gmail.com>
2023-09-10 22:56:06 -0400

    fix button not showing disabled

--- src/components/atomic/Button.tsx
+++ src/components/atomic/Button.tsx
@@ -4,16 +4,11 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children?: React.ReactNode;
 }
 
-export default function Button({
-  children,
-  className,
-  disabled,
-  ...props
-}: ButtonProps) {
+export default function Button({ children, className, ...props }: ButtonProps) {
   return (
     <button
       {...props}
-      className={`transition-100 whitespace-nowrap border-2 border-primary bg-background px-3 py-2 text-primary transition-colors hover:underline disabled:border-foreground disabled:text-foreground ${className}`}
+      className={`transition-100 whitespace-nowrap border-2 border-primary bg-background px-3 py-2 text-primary transition-colors hover:underline disabled:border-foreground disabled:text-foreground disabled:no-underline ${className}`}
     >
       {children}
     </button>
