f025c8f170780e490ed3f24a92cfdf5c45f01a42
Martin Miglio <marmig0404@gmail.com>
2023-09-10 23:02:31 -0400

    link report frontend to api

--- src/components/ReportInput.tsx
+++ src/components/ReportInput.tsx
@@ -1,16 +1,66 @@
-import InputSelector from "@/components/InputSelector";
-import ItemSearch from "@/components/ItemSearch";
+"use client";
+
+import InputSelector, { InputItem } from "@/components/InputSelector";
+import ItemSearch, { ReturnItem } from "@/components/ItemSearch";
 import Button from "@/components/atomic/Button";
+import { useState } from "react";
 
 export default function ReportInput() {
+  const [inputItem, setInputItem] = useState<InputItem | null>(null);
+  const [returnItems, setReturnItems] = useState<ReturnItem[]>([]);
+  const [submitting, setSubmitting] = useState(false);
+
+  const onSubmit = async () => {
+    if (!inputItem) {
+      return;
+    }
+
+    setSubmitting(true);
+
+    const newReport = {
+      input_item_id: inputItem?.itemId,
+      returned_item_ids: returnItems.map((item) => item.id),
+      cost: inputItem?.value,
+      value: returnItems.reduce(
+        (acc, item) => acc + (item.avg24hPrice ?? 0),
+        0,
+      ),
+      patch: process.env.NEXT_PUBLIC_GAME_PATCH,
+    };
+
+    const res = await fetch("/api/report", {
+      method: "POST",
+      body: JSON.stringify(newReport),
+    });
+
+    if (res.ok) {
+      console.info("Report submitted successfully");
+    } else if (res.status === 403) {
+      console.warn("Report not submitted, rate limited");
+    } else if (res.status === 401) {
+      console.warn("Report not submitted, not logged in");
+    } else {
+      console.error(
+        `Report not submitted, error ${res.status}: ${res.statusText}`,
+      );
+    }
+
+    setSubmitting(false);
+  };
+
   return (
     <div className="flex flex-col gap-2">
       <span className="flex items-center justify-between">
         <h2 className="text-2xl">Report a scav case</h2>
-        <Button>Submit</Button>
+        <Button
+          onClick={onSubmit}
+          disabled={!inputItem || returnItems.length === 0 || submitting}
+        >
+          Submit
+        </Button>
       </span>
-      <InputSelector />
-      <ItemSearch />
+      <InputSelector setSelectedItem={setInputItem} />
+      <ItemSearch setSelectedItems={setReturnItems} />
     </div>
   );
 }
