1d8f5f8f4bbe0f9a8f14714a6123f6d8e5c00716
Martin Miglio <marmig0404@gmail.com>
2023-09-11 21:01:44 -0400

    disable image optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "assets.tarkov.dev" },
       { protocol: "https", hostname: "via.placeholder.com" },
